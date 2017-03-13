1218ac6ada1c41b93c08d96036100c1f5dffee5b
felix-developer <felixbosshard@icloud.com>
2017-03-13 20:52:12 +0000

    remove the pissing part

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,5 @@ class ApplicationController < ActionController::Base
     render html: "hello world, this is my first application. How freaking awesome is that... 
     Dont you think? </br> <p>this is a paragraph</p> 
     <h1>Das ist jetzt gross</h1> ".html_safe()
-    
-    # Ich muss kurz pissen :) .html_safe()
   end
 end
